939b44aa520b70f458c40d58eedcfe966b971f77
Andrew <andreww.pataleta@gmail.com>
2025-08-14 12:00:49 +0300

    Fix navigation and remove press effect in chat creation

--- app/src/main/java/uddug/com/naukoteka/ui/chat/compose/ChatCreateSingleScreen.kt
+++ app/src/main/java/uddug/com/naukoteka/ui/chat/compose/ChatCreateSingleScreen.kt
@@ -3,6 +3,7 @@ package uddug.com.naukoteka.ui.chat.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,9 +48,7 @@ fun ChatCreateSingleScreen(
     ) {
         ChatToolbarCreateSingleComponent(
             viewModel = viewModel,
-            onBackPressed = {
-
-            }
+            onBackPressed = onBackPressed,
         )
 
         when (uiState) {
@@ -65,7 +65,10 @@ fun ChatCreateSingleScreen(
                 Row(
                     modifier = Modifier
                         .padding(20.dp)
-                        .clickable {
+                        .clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null,
+                        ) {
                             onGroupCreateClick()
                         },
                     verticalAlignment = Alignment.CenterVertically
