a1b839fd5b60bb66c08b0ea7be4a21cc23e09ac1
Andrew <andreww.pataleta@gmail.com>
2025-08-18 11:10:24 +0300

    Add chat domain repository and interactor

--- app/src/main/java/uddug/com/naukoteka/ui/chat/di/ChatModule.kt
+++ app/src/main/java/uddug/com/naukoteka/ui/chat/di/ChatModule.kt
@@ -5,11 +5,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import retrofit2.Retrofit
-import uddug.com.data.NaukotekaCookieJar
 import uddug.com.data.cache.cookies.CookiesCache
-import uddug.com.data.repositories.chat.ChatRepository
 import uddug.com.data.repositories.chat.ChatRepositoryImpl
 import uddug.com.data.services.chat.ChatApiService
+import uddug.com.domain.interactors.chat.ChatInteractor
+import uddug.com.domain.repositories.chat.ChatRepository
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -22,9 +22,10 @@ object ChatModule {
 
 
     @Provides
-    fun provideChatRepository(apiService: ChatApiService): ChatRepository {
-        return ChatRepositoryImpl(apiService)
-    }
+    fun provideChatRepository(apiService: ChatApiService): ChatRepository = ChatRepositoryImpl(apiService)
+
+    @Provides
+    fun provideChatInteractor(chatRepository: ChatRepository): ChatInteractor = ChatInteractor(chatRepository)
 
     @Provides
     fun provideSocketService(cookiesCache: CookiesCache): SocketService {
