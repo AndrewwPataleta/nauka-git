0f2d16d213b67f4c7960e724b17588285ab44a0c
Andrew <andreww.pataleta@gmail.com>
2025-10-01 14:28:15 +0300

    Ensure group dialog roles include non-admin members

--- data/src/main/java/uddug/com/data/services/models/request/chat/CreateDialogRequestDto.kt
+++ data/src/main/java/uddug/com/data/services/models/request/chat/CreateDialogRequestDto.kt
@@ -1,11 +1,12 @@
 package uddug.com.data.services.models.request.chat
 
+import com.google.gson.JsonObject
 import com.google.gson.annotations.SerializedName
 
 data class CreateDialogRequestDto(
     @SerializedName("dialogName") val dialogName: String?,
     @SerializedName("dialogImage") val dialogImage: DialogImageRequestDto?,
-    @SerializedName("userRoles") val userRoles: Map<String, String?>
+    @SerializedName("userRoles") val userRoles: JsonObject
 )
 
 data class DialogImageRequestDto(
